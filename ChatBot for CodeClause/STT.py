9ed8175a4c273a2679a8c599a214e54d3a6de66f
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2022-05-08 22:05:02 +0530

    Commit 2.0
    Finished with speech and text input output and generate algorithm for chatbot

--- ChatBot for CodeClause/STT.py
+++ ChatBot for CodeClause/STT.py
@@ -1,9 +1,8 @@
-from TTS import *
+from xmlrpc.client import MAXINT
 import speech_recognition as sr
 import pyaudio 
+from TTS import *
 import pywhatkit as pwt
-from Clicker import *
-from VideoDownloader import *
 from speech_recognition import *
 
 def stt():
@@ -12,18 +11,8 @@ def stt():
         with sr.Microphone(device_index=2) as source:
             # audio_text=r.adjust_for_ambient_noise(source,5)
             # audio=r.listen(source=source,phrase_time_limit=5)
-            print("Speech to text : ")
             audio = r.listen(source)
-            try:
             # using google speech recognition
-                text = r.recognize_google(audio)
-                print(text)
-                t=pwt.playonyt(text)
-                print(t)
-                return
-            except:
-                tts('Couldnt Recognize your voice')
-                return
-            
-def playvideo(video):
-    pwt.playonyt(video)
+            text = r.recognize_google(audio)
+            print(text)
+            return text
